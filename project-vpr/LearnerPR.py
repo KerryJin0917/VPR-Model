042f06ca2ed9feca9e6c8d0c382dd26b9734e431
Kerry Jin <jinkerry@unc.edu>
2026-04-11 21:31:30 -0400

    LearnerPR changes

--- project-vpr/LearnerPR.py
+++ project-vpr/LearnerPR.py
@@ -153,15 +153,22 @@ class TripletLoss(nn.Module):
 class TrainableModel(nn.Module):
     def __init__(self, embedding_dim=512):
         super().__init__()
-        # Load DINOv2-Small (vits14)
-        self.backbone = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+        # Load the code locally from your project folder
+        self.backbone = torch.hub.load(
+            repo_or_dir='/nas/longleaf/home/jinkerry/project-vpr/dinov2',
+            model='dinov2_vits14',
+            source='local',
+            pretrained=False
+        )
 
-        # DINOv2-Small has a 384-dim output.
-        # We add a projection head to reach your desired embedding_dim.
+        # Load the weights you uploaded to your weights folder
+        state_dict = torch.load('./weights/dinov2_vits14_pretrain.pth', map_location='cpu')
+        self.backbone.load_state_dict(state_dict)
+
+        # Head stays the same (384 for DINOv2-Small)
         self.head = nn.Sequential(
             nn.Linear(384, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(512, embedding_dim)
         )
 
